bac926d90a58a859f048134a71c228324f11dc19
Kinshuk <kinshuksinha91@yahoo.co.in>
2016-02-26 16:15:45 -0800

    submiting flask lab 2

---
+++ flaskLab2/app/static/interactions.js
@@ -0,0 +1,41 @@
+$('#submit-survey').on('click', function submitSurvey() {
+	var color = $("input[name=color]").val();
+	var food = $("input[name=food]").val();
+	var vacation = $("input[name=vacation]").val();
+	var feBefore = $("input[name=front-end-before]").val();
+	var feAfter = $("input[name=front-end-after]").val();
+	console.log("here")
+	$.post('/submit-survey', 
+		{ food: food,
+		color: color,
+		vacation: vacation,
+		before:feBefore,
+		after:feAfter }, 
+		function (data) {
+    	//console.log(data);
+    	document.body.parentNode.innerHTML=data;
+	});
+
+});
+
+$("#results-email-container").on('click', '#email-results-button', function emailResults() {
+	console.log($(this));
+});
+
+$("#site-title-wrapper").on('click', function goHome() {
+	window.location.href = '/';
+});
+
+$(document).ready(function applySliderLabels() {
+	var currentValue = $("#fe-before").val();
+	$("#fe-before").next().html(currentValue);
+
+	currentValue = $("#fe-after").val();
+	$("#fe-after").next().html(currentValue);
+});
+
+
+$("input[type='range']").on('change', function updateLabel() {
+	var currentValue = $(this).val();
+	$(this).next().html(currentValue);
+});
